735ea2660764a4b71e2871783f036ae41c032c2a
student <student@DESKTOP-097MIS6>
2025-11-29 12:05:45 +0300

    proekt

---
+++ src/chaikans_13/ChaikaNS_13.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chaikans_13;
+
+/**
+ *
+ * @author student
+ */
+public class ChaikaNS_13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         char ch;
+
+
+        ch = 'x' ;
+        System.out.println("ch contains " + ch) ;
+
+
+        ch++; // переменную типа char можно инкрементировать
+        System.out.println("ch is now " + ch) ;
+
+
+        ch = 90; // переменной типа char можно присвоить целочисленное значение
+        System.out.println("ch is now " + ch) ;
+    }
+}
+
+    
+    
